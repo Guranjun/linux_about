2b941b209f63bdc16527380ee28f3108f9139309
Guranjun <13868382860@163.com>
2026-05-25 17:42:10 +0800

    add:qt项目初始化

---
+++ QT/Desktop/widget.cpp
@@ -0,0 +1,16 @@
+#include "widget.h"
+#include "./ui_widget.h"
+#include <QPushButton>
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    QPushButton * quitBtn = new QPushButton("关闭窗口",this);
+    connect(quitBtn,&QPushButton::clicked,this,&QWidget::close);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
